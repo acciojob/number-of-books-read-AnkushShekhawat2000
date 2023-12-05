04d3b649d8933ba3f5ef1a81e5e410d90b483676
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2023-12-05 16:05:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+// const library = [
+//   {
+//     author: "Bill Gates",
+//     title: "The Road Ahead",
+//     readingStatus: true,
+//   },
+//   {
+//     author: "Steve Jobs",
+//     title: "Walter Isaacson",
+//     readingStatus: true,
+//   },
+//   {
+//     author: "Suzanne Collins",
+//     title: "Mockingjay: The Final Book of The Hunger Games",
+//     readingStatus: false,
+//   },
+// ];
+
+// const numberOfBooksRead = () => {
+//   // write your code here
+// };
+
+// // Do not change the code below
+
+// alert(numberOfBooksRead());
+
+
+
+
+
 const library = [
   {
     author: "Bill Gates",
@@ -17,9 +47,8 @@ const library = [
 ];
 
 const numberOfBooksRead = () => {
-  // write your code here
+  return library.filter(book => book.readingStatus === true).length;
 };
 
-// Do not change the code below
-
+// Example
 alert(numberOfBooksRead());
